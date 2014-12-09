168e569bb312cdd13cfbfc6aef3989c12195520b
mrhallberg <mr.hallberg@hotmail.com>
2014-12-09 11:00:12 +0100

    User Classes

--- Tursit app V5/model/Userlist.cs
+++ Tursit app V5/model/Userlist.cs
@@ -9,16 +9,38 @@ namespace Tursit_app_V5.model
 {
     class Userlist
     {
-        public ObservableCollection<User> LisOfUsers { get; set; }
+        public ObservableCollection<User> ListOfUsers { get; set; }
+
+        public User CurrentUser = null;
 
         public Userlist(ObservableCollection<User> lisOfUsers)
         {
-            LisOfUsers = lisOfUsers;
+            ListOfUsers = lisOfUsers;
+        }
+
+        public Boolean Check(string name, string password)
+        {
+            foreach (var user in ListOfUsers)
+            {
+                if (user.Name.Equals(name) && user.Password.Equals(password))
+                {
+                    CurrentUser = user;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public Boolean CreateUser(string name, string gender, string password, int age, int numberOfChildren, string relationship)
+        {
+            if (ListOfUsers.Any(user => user.Name.Equals(name))) return false;
+            ListOfUsers.Add(new User(name, gender, password, age, numberOfChildren, relationship));
+            return true;
         }
 
         public override string ToString()
         {
-            return string.Format("LisOfUsers: {0}", LisOfUsers);
+            return string.Format("LisOfUsers: {0}", ListOfUsers);
         }
     }
 }
---
+++ Tursit app V5/viewmodel/Singleton.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tursit_app_V5.model;
+
+namespace Tursit_app_V5.viewmodel
+{
+    class Singleton
+    {
+        private static Singleton instance = null;
+
+        public MainViewModel ViewModel;
+        public Userlist UserList;
+
+        private Singleton()
+        {
+            ViewModel = new MainViewModel();
+            UserList = new Userlist(getUserData());
+        }
+
+        public static Singleton Instance
+        {
+            get { return instance ?? (instance = new Singleton()); }
+        }
+
+        private ObservableCollection<User> getUserData()
+        {
+            return new ObservableCollection<User>
+            {
+                new User("Søren", "Male", "1234", 32, 2, "Married"),
+                new User("Malene", "Female", "1234", 25, 0, "Single"),
+                new User("Karen", "Female", "1234", 56, 2, "Divorced")
+            };
+        } 
+    }
+}
